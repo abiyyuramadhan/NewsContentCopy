5716c7580e8deee2332893a5212da001cdc77926
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-01-30 21:47:46 +0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('views'))
+app.set('view engine', 'ejs')
+
+
+const rootRoute = require('./route/rootRoute')
+app.use(rootRoute)
+
+
+app.listen(port, ()=>{
+    console.log(`listening at http://localhost:${port}`);
+})
